c6c154571bece52e9974cfc37b5e423499272d1a
Lex-KZ <azaskalkina@hotmail.com>
2021-07-20 20:22:35 +1000

    To add test folder and nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>Our cakes</li>
+          <li>About Us</li>
+          <li>Enquiries</li>
+          {/* {
+            if (loggedIn){
+              return(
+                <li>Profile</li>
+                <li>Log Out</li>
+              );
+            } else {
+              <li>Log In</li>
+            }
+          } */}
+        </ul>
+      </nav>
+      <div id="landing-page">
+        <img alt="Sprinkles of Joy cupcake logo" ></img>
+        <h1>Sprinkles of Joy</h1>
+        <button>View Our Creations</button>
+      </div>
     </div>
   );
 }
