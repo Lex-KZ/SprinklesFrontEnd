124cb2c88bdb5c14cec097acaa3aa388966fadcc
Lex-KZ <azaskalkina@hotmail.com>
2021-07-27 15:32:51 +1000

    To add styling to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,23 @@ import cupcake from '.././caitlyn-de-wild-9I4Oe-atXck-unsplash.jpg'
 
 function About() {
     return(
-        <div>
-            <h1>About Us</h1>
-            <img src={cupcake} alt="pink cupcake" width="60%"></img>
-            <p>We are a small independent artisanal cake bakery based in Sydney. We craft unique, customised cakes for all 
-            occasions; Birthdays, Weddings, Graduations, Anniversaries. You name it, we craft for it!</p>
-            <p>Our cakes are made from the highest quality ingredients. We take great care to make sure that our cakes 
-            are made from ingredients from renewable and conscientious sources.</p>
-            <p>Book an appointment with us to discuss events that need a sprinkle of joy!</p>
+        <div className="flex justify-center">
+            <div className="max-w-2xl rounded overflow-hidden shadow-lg my-4 mx-4">
+                <img src={cupcake} alt="pink cupcake" className="w-full"></img>
+                <div className="flex justify-center">
+                    <p className="font-bold text-xl mb-2 mt-4">About Us</p>
+                </div>
+                <div className="px-6 py-4">
+                    <p>We are a small independent artisanal cake bakery based in Sydney. We craft unique, customised cakes for all 
+                    occasions; Birthdays, Weddings, Graduations, Anniversaries. You name it, we craft for it!</p>
+                    <br />
+                    <p>Our cakes are made from the highest quality ingredients. We take great care to make sure that our cakes 
+                    are made from ingredients from renewable and conscientious sources.</p>
+                    <br />
+                    <p>Book an appointment with us to discuss events that need a sprinkle of joy!</p>
+                    <br />
+                </div>
+            </div>
         </div>
     )
 }
