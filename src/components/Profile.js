391a7c066dd311cdae9294189373249bf435e8ba
Lex-KZ <azaskalkina@hotmail.com>
2021-07-27 22:23:14 +1000

    To add styling to Profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -29,26 +29,41 @@ const Profile = () => {
     return(
         user ?
             (
-                <div>
-                    <h1>User Profile</h1>
-                    <p>Username: {user.username}</p>
-                    <p>Name: {`${user.first_name} ${user.last_name}`}</p>
-                    <p>Email: {user.email}</p>
-                    <h2>Enquiries</h2>     
-                       <div>                
-                           {
-                               enquiries.map(enquiry => (
-                                       <div key={enquiry.id}>
-                                           <p>
-                                               {enquiry.id}
-                                               {enquiry.topic}
-                                               {enquiry.message}
-                                           </p>
-                                       </div>
-                          
-                               ))
-                           }
-                       </div>
+                <div className="flex justify-center ">
+                    <div className="max-w-2xl rounded overflow-hidden shadow-lg my-8 mx-4 py-8 px-16">
+                        <div className="flex justify-center">
+                            <p className="font-bold text-xl mb-2 mt-4">User Profile</p>
+                        </div>
+                        <p className="px-6 py-4">Username: {user.username}</p>
+                        <p className="px-6 py-4">Name: {`${user.first_name} ${user.last_name}`}</p>
+                        <p className="px-6 py-4">Email: {user.email}</p>
+                        <h2 className="font-bold text-xl mb-2 mt-4">Enquiries:</h2>     
+                        <div >                
+                            {
+                                enquiries.map(enquiry => (
+                                        <div className="w-screen">
+                                            <table className="table-auto min-w-max divide-y divide-gray-200">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col" className="w-1/4 px-4">ID</th>
+                                                            <th scope="col" className="w-1/4 px-4">Topic</th>
+                                                            <th scope="col" className="w-1/2 px-4">Message</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr key={enquiry.id}>
+                                                            <td className="text-center">{enquiry.id}</td>
+                                                            <td className="text-center">{enquiry.topic}</td>
+                                                            <td className="text-center">{enquiry.message}</td>
+                                                        </tr>
+                                                    </tbody>
+                                            </table>
+                                        </div>
+                            
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
             ) : (
                 <div>Loading...</div>
